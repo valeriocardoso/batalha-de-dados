d0bcc6dda8a48a63680fb008372620c0c6dff861
arcursino <arcursino@gmail.com>
2021-05-29 19:32:11 -0300

    create a teacher and student profiles

--- app.py
+++ app.py
@@ -1,97 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-# DB Management
-import sqlite3 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-
-
-# Security
-#passlib,hashlib,bcrypt,scrypt
-import hashlib
-def make_hashes(password):
-	return hashlib.sha256(str.encode(password)).hexdigest()
-
-def check_hashes(password,hashed_text):
-	if make_hashes(password) == hashed_text:
-		return hashed_text
-	return False
-
-
-
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
-
-
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-	conn.commit()
-
-def login_user(username,password):
-	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-	data = c.fetchall()
-	return data
-
-
-def view_all_users():
-	c.execute('SELECT * FROM userstable')
-	data = c.fetchall()
-	return data
-
-
-
 def main():
-	"""Simple Login App"""
+	"""BATALHA DE DADOS"""
 
-	st.title("Simple Login App")
+	st.title("BATALHA DE DADOS")
 
-	menu = ["Home","Login","SignUp"]
+	menu = ["Home","Estudante","Professor(a)"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
 	if choice == "Home":
 		st.subheader("Home")
 
-	elif choice == "Login":
-		st.subheader("Login Section")
+	elif choice == "Estudante":
+		st.subheader("Seção do Estudante")
+		
+		if st.sidebar.checkbox("Estudante"):
+			st.subheader("Desafio do Estudante")			
 
-		username = st.sidebar.text_input("User Name")
-		password = st.sidebar.text_input("Password",type='password')
-		if st.sidebar.checkbox("Login"):
-			# if password == '12345':
-			create_usertable()
-			hashed_pswd = make_hashes(password)
+	elif choice == "Professor(a)":
+		st.subheader("Seção do(a) Professor(a)")
 
-			result = login_user(username,check_hashes(password,hashed_pswd))
-			if result:
-
-				st.success("Logged In as {}".format(username))
-
-				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-				if task == "Add Post":
-					st.subheader("Add Your Post")
-
-				elif task == "Analytics":
-					st.subheader("Análise da turma")
-				elif task == "Profiles":
-					st.subheader("User Profiles")
-					user_result = view_all_users()
-					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-					st.dataframe(clean_db)
-			else:
-				st.warning("Incorrect Username/Password")
-
-	elif choice == "SignUp":
-		st.subheader("Create New Account")
-		new_user = st.text_input("Username")
-		new_password = st.text_input("Password",type='password')
-
-		if st.button("Signup"):
-			create_usertable()
-			add_userdata(new_user,make_hashes(new_password))
-			st.success("You have successfully created a valid Account")
-			st.info("Go to Login Menu to login")
-
+		if st.sidebar.checkbox("Professor(a)"):
+			st.subheader("Diário de Classe")			
 
 
 if __name__ == '__main__':
